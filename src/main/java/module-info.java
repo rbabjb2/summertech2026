3ec6228b7d864b80d2a891b62df264b09aa3ef94
rbabjb2 <sblbrbabjb2@gmail.com>
2026-07-14 12:44:09 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.summertech2026 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.json;
-
-    opens com.summertech2026 to javafx.fxml;
-    exports com.summertech2026;
-}
